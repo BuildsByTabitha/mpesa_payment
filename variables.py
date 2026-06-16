1cecc444062286720b4ad383c8c5cd4b4f501e6c
BuildsByTabitha <tabithanjeriis19@gmail.com>
2026-06-16 21:37:04 +0300

    DAY 01:VARIABLES

---
+++ variables.py
@@ -0,0 +1,17 @@
+item_name = "A"
+
+# 2. Create an Integer variable for how much that item costs in Ksh
+item_cost = 250000
+
+# 3. Create a String variable for a Safaricom phone number starting with 254
+customer_phone = "9325472"
+
+# 4. Create a Boolean variable indicating if the payment has been received yet
+payment_received = True
+
+# This will print your variables to the screen so you can see them work!
+print("Invoice Details:")
+print("Item:", item_name)
+print("Amount due Ksh:", item_cost)
+print("Send prompt to:", customer_phone)
+print("Is it paid yet?", payment_received)
