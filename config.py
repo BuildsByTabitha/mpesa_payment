b15421ec07f162b131b0bb157a23e4e0eb161bbc
BuildsByTabitha <tabithanjeriis19@gmail.com>
2026-06-17 23:56:55 +0300

    Into to configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+# our M-Pesa Configuration Variables
+
+MPESA_CONSUMER_KEY = "dummy_consumer_key"
+MPESA_CONSUMER_SECRET = "dummy_consumer_secret"
+BUSINESS_SHORTCODE = "174379"
+PASSKEY = "dummy_safaricom_passkey"
+
+print("✅ Pushing this one fast to get my green square of the Day but will get back in a few with details.")
